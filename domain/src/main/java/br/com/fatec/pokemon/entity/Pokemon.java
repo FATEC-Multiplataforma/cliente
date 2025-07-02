2ec2e40e2f26f3f25aefa3fca64ac8fcd8627557
MrSpock182 <klebernunes182@gmail.com>
2025-07-01 23:50:32 -0300

    criacao da aula 5

--- domain/src/main/java/br/com/fatec/pokemon/entity/Pokemon.java
+++ domain/src/main/java/br/com/fatec/pokemon/entity/Pokemon.java
@@ -1,10 +1,11 @@
 package br.com.fatec.pokemon.entity;
 
+import java.io.Serializable;
 import java.util.List;
 
 public record Pokemon(
         String name,
         Integer weight,
         List<PokemonType> types
-) {
+) implements Serializable {
 }
--- springframework/src/main/java/br/com/fatec/pokemon/controller/CacheController.java
+++ springframework/src/main/java/br/com/fatec/pokemon/controller/CacheController.java
@@ -1,30 +1,30 @@
-package br.com.fatec.pokemon.controller;
-
-import com.github.benmanes.caffeine.cache.Cache;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.caffeine.CaffeineCache;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@RestController
-public class CacheController {
-    private final CacheManager cacheManager;
-
-    public CacheController(CacheManager cacheManager) {
-        this.cacheManager = cacheManager;
-    }
-
-    @GetMapping("/cache")
-    public Map<Object, Object> cache() {
-        CaffeineCache cache = (CaffeineCache) cacheManager.getCache("pokemon-cache");
-        if (cache == null) {
-            return Map.of();
-        }
-        Cache<Object, Object> nativeCache = cache.getNativeCache();
-        return new HashMap<>(nativeCache.asMap());
-    }
-
-}
+//package br.com.fatec.pokemon.controller;
+//
+//import com.github.benmanes.caffeine.cache.Cache;
+//import org.springframework.cache.CacheManager;
+//import org.springframework.cache.caffeine.CaffeineCache;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@RestController
+//public class CacheController {
+//    private final CacheManager cacheManager;
+//
+//    public CacheController(CacheManager cacheManager) {
+//        this.cacheManager = cacheManager;
+//    }
+//
+//    @GetMapping("/cache")
+//    public Map<Object, Object> cache() {
+//        CaffeineCache cache = (CaffeineCache) cacheManager.getCache("pokemon-cache");
+//        if (cache == null) {
+//            return Map.of();
+//        }
+//        Cache<Object, Object> nativeCache = cache.getNativeCache();
+//        return new HashMap<>(nativeCache.asMap());
+//    }
+//
+//}
