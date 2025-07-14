2a64e8df19a1f5dc713b3c51a2052d61fd96806a
MrSpock182 <klebernunes182@gmail.com>
2025-07-14 15:00:00 -0300

    adicionando cache para docker

--- springframework/src/main/java/br/com/fatec/pokemon/PokemonApplication.java
+++ springframework/src/main/java/br/com/fatec/pokemon/PokemonApplication.java
@@ -1,5 +1,7 @@
 package br.com.fatec.pokemon;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,8 +12,18 @@ import org.springframework.retry.annotation.EnableRetry;
 @EnableCaching
 @SpringBootApplication
 @EnableFeignClients(basePackages = "br.com.fatec.pokemon.integration.client")
-public class PokemonApplication {
+public class PokemonApplication implements CommandLineRunner {
+    @Value("${spring.redis.host}")
+    String host;
+    @Value("${spring.redis.port}")
+    String port;
+
     public static void main(String[] args) {
         SpringApplication.run(PokemonApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Connecting to Redis at Host: " + host + " Port:" + port);
     }
 }
--- springframework/src/main/java/br/com/fatec/pokemon/configuration/CacheConfig.java
+++ springframework/src/main/java/br/com/fatec/pokemon/configuration/CacheConfig.java
@@ -1,10 +1,13 @@
 package br.com.fatec.pokemon.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -13,6 +16,14 @@ import java.time.Duration;
 
 @Configuration
 public class CacheConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.port}") Integer port) {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new LettuceConnectionFactory(config);
+    }
 
     @Bean
     public RedisCacheManager redisCacheManager(RedisConnectionFactory factory) {
@@ -26,15 +37,4 @@ public class CacheConfig {
                 .cacheDefaults(config)
                 .build();
     }
-
-//    @Bean
-//    public CacheManager cacheManager() {
-//        CaffeineCacheManager cacheManager = new CaffeineCacheManager("endereco-cache");
-//        cacheManager.setCaffeine(
-//                Caffeine.newBuilder()
-//                        .expireAfterWrite(60, TimeUnit.SECONDS)
-//                        .maximumSize(1000));
-//        return cacheManager;
-//    }
-
 }
